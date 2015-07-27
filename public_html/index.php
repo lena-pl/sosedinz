0ddc753183a4316df5a3bdbb4570dd4273c947ac
Lena Plaksina <lena.plaksina@gmail.com>
2015-07-28 10:04:23 +1200

    Refactor config files for security

---
+++ public_html/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require "vendor/autoload.php";
+
+session_start();
+session_regenerate_id(true);
+
+$auth = new App\Services\AuthenticationService();
+
+App\Views\View::registerAuthenticationService($auth);
+App\Controllers\Controller::registerAuthenticationService($auth);
+
+
+require "routes.php";
--- sosedinz-config-sample.inc.php
+++ sosedinz-config-sample.inc.php
@@ -10,23 +10,26 @@ if (stristr($_SERVER['HTTP_HOST'], ".yoobee.net.nz")) {
     ini_set('display_errors', true);
     ini_set("log_errors", 1);
     ini_set("error_log", getcwd() . "/php-error.log");
+
+    define("MAILGUN_KEY", '');
+    define("MAILGUN_DOMAIN", '');
+
+    define("DB_HOST", '');
+    define("DB_NAME", '');
+    define("DB_USER", '');
+    define("DB_PASS", '');
 } else {
     define("DEV_ENVIRONMENT", true);
     // local developer environment
     ini_set('display_errors', true);
     ini_set("log_errors", 1);
     ini_set("error_log", getcwd() . "/php-error.log");
+
+    define("MAILGUN_KEY", '');
+    define("MAILGUN_DOMAIN", '');
+
+    define("DB_HOST", '');
+    define("DB_NAME", '');
+    define("DB_USER", '');
+    define("DB_PASS", '');
 }
-
-require "vendor/autoload.php";
-
-session_start();
-session_regenerate_id(true);
-
-$auth = new App\Services\AuthenticationService();
-
-App\Views\View::registerAuthenticationService($auth);
-App\Controllers\Controller::registerAuthenticationService($auth);
-
-
-require "routes.php";
