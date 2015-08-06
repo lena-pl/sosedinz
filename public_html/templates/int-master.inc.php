7f8cade95c72b2e7110963bbfc2e3afe45f0352f
Lena Plaksina <lena.plaksina@gmail.com>
2015-08-06 13:53:15 +1200

    Fix internal nav bar functionality on mobile screens

--- public_html/templates/int-master.inc.php
+++ public_html/templates/int-master.inc.php
@@ -23,57 +23,52 @@
     <div class="container">
 
 <!-- ======================================================================================= -->
-              <!-- NEW NAV -->
+              <!-- HOME NAV -->
 <!-- ======================================================================================= -->
-      <nav class="navbar navbar-default" role="navigation">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="./?page=dash">sosediNZ</a>
-        </div>
-
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-          </ul>
-              <div class="col-sm-3 col-md-3">
-                  <form method="GET" action="./" class="navbar-form navbar-right" role="search">
-                    <div class="form-group input-group">
-                      <input type="hidden" name="page" value="search">
-                      <input name="q" type="search" class="form-control" placeholder="Search">
-                        <span class="input-group-btn"><button type="submit" class="btn btn-default" aria-label="Search">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="./">sosediNZ</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+            </ul>
+            <div class="col-sm-3 col-md-3">
+                <form method="GET" action="./" class="navbar-form navbar-right" role="search">
+                  <div class="form-group input-group">
+                    <input type="hidden" name="page" value="search">
+                    <input name="q" type="search" class="form-control" placeholder="Search">
+                      <span class="input-group-btn">
+                        <button type="submit" class="btn btn-default" aria-label="Search">
                           <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
                         </button>
                       </span>
-                    </div>
-                  </form>
-              </div>
-          <ul class="nav navbar-nav navbar-right">
-
-            <?php if (! static::$auth->check()): ?>
-              <li><a href="./">Login</a></li>
-              <li <?php if ($page === "auth.register"): ?> class="active" <?php endif; ?>><a href="./?page=register">Register</a></li>
-            <?php else: ?>
-            <li><a href="./?page=post.create">New Post</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?= static::$auth->user()->username; ?> <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="./?page=home">Dashboard</a></li>
-                <li><a href="./?page=account.edit">Edit Account</a></li>
-                <li class="divider"></li>
-                <li><a href="./?page=logout">Logout</a></li>
-            <?php endif; ?>
-
-              </ul>
-            </li>
-          </ul>
-        </div><!-- /.navbar-collapse -->
+                  </div>
+                </form>
+            </div>
+            <ul class="nav navbar-nav navbar-right">
+              <?php if (! static::$auth->check()): ?>
+                <li><a href="./">Login</a></li>
+                <li <?php if ($page === "auth.register"): ?> class="active" <?php endif; ?>><a href="./?page=register">Register</a></li>
+              <?php else: ?>
+              <li><a href="./?page=post.create">New Post</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?= static::$auth->user()->username; ?> <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="./?page=home">Dashboard</a></li>
+                  <li><a href="./?page=account.edit">Edit Account</a></li>
+                  <li class="divider"></li>
+                  <li><a href="./?page=logout">Logout</a></li>
+              <?php endif; ?>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
       </nav>
 
       <?php $this->content (); ?>
