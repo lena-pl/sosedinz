1124d80c68182b4f608675728f54d25e42e0021c
Lena Plaksina <lena.plaksina@gmail.com>
2015-08-06 13:53:25 +1200

    Pad content in home view on mobile

--- public_html/templates/index.inc.php
+++ public_html/templates/index.inc.php
@@ -11,61 +11,69 @@
       </div>
 <!--==================================== LOGIN FORM =======================================-->
       <div class="row">
-        <div class="col-sm-4 col-sm-offset-4">
-          <form method="POST" action="./?page=auth.attempt" class="form-horizontal">
+        <div class="container">
+          <div class="col-sm-4 col-sm-offset-4">
+            <form method="POST" action="./?page=auth.attempt" class="form-horizontal">
 
-            <?php if ($error): ?>
-              <div class="alert alert-warning" role="alert"><strong>Warning:</strong> No user with that email and password combination was found. Check spelling/capitalisation and try again.</div>
-            <?php endif; ?>
+              <?php if ($error): ?>
+                <div class="alert alert-warning" role="alert"><strong>Warning:</strong> No user with that email and password combination was found. Check spelling/capitalisation and try again.</div>
+              <?php endif; ?>
 
-            <div class="form-group form-group-lg<?php if ($errors['email']): ?> has-error <?php endif; ?>">
-              <div>
-                <input id="email" class="form-control input-lg" name="email"
-                  placeholder="Email Address"
-                  value="<?= $user->email; ?>">
-                <div class="help-block"><?= $errors['email']; ?></div>
+              <div class="form-group form-group-lg<?php if ($errors['email']): ?> has-error <?php endif; ?>">
+                <div>
+                  <input id="email" class="form-control input-lg" name="email"
+                    placeholder="Email Address"
+                    value="<?= $user->email; ?>">
+                  <div class="help-block"><?= $errors['email']; ?></div>
+                </div>
               </div>
-            </div>
 
-            <div class="form-group form-group-lg<?php if ($errors['password']): ?> has-error <?php endif; ?>">
-              <div>
-                <input id="password" class="form-control input-lg" name="password" type="password" placeholder="Password">
-                <div class="help-block"><?= $errors['password']; ?></div>
+              <div class="form-group form-group-lg<?php if ($errors['password']): ?> has-error <?php endif; ?>">
+                <div>
+                  <input id="password" class="form-control input-lg" name="password" type="password" placeholder="Password">
+                  <div class="help-block"><?= $errors['password']; ?></div>
+                </div>
               </div>
-            </div>
 
 
-            <div class="form-group">
-              <div>
-                <button class="btn btn-block btn-success">
-                  <span class="glyphicon glyphicon-ok"></span> Log in
-                </button>
+              <div class="form-group">
+                <div>
+                  <button class="btn btn-block btn-success">
+                    <span class="glyphicon glyphicon-ok"></span> Log in
+                  </button>
+                </div>
               </div>
-            </div>
-          </form>
+            </form>
 
-        </div>
-      </div>
+          </div>
+        </div> <!-- /Container -->
+      </div> <!-- /Row -->
 
 <!-- ============================================================================================= -->
 
       <div class="row">
-        <div class="col-sm-6 col-sm-offset-3">
-          <p>Sosedi (sa-sé-dee) means “neighbours” in Russian and sosediNZ aims to bring together neighbours from all walks of life. It’s a blogging network that connects Russian New Zealanders, their friends, family and anyone else who wants to learn more about the Russian culture or life in New Zealand.</p>
-        </div>
-      </div>
+        <div class="container">
+          <div class="col-sm-6 col-sm-offset-3">
+            <p>Sosedi (sa-sé-dee) means “neighbours” in Russian and sosediNZ aims to bring together neighbours from all walks of life. It’s a blogging network that connects Russian New Zealanders, their friends, family and anyone else who wants to learn more about the Russian culture or life in New Zealand.</p>
+          </div>
+        </div> <!-- /Container -->
+      </div> <!-- /Row -->
 
       <div class="row">
-        <div class="col-sm-4">
-          <p class="lead text-center">Blog</p>
-          <p>Publish your own blog posts about your experiences in New Zealand and connect with other Russians.</p>
-        </div>
-        <div class="col-sm-4">
-          <p class="lead text-center">Comment</p>
-          <p>Share your thoughts and discuss other people’s posts.</p>
-        </div>
-        <div class="col-sm-4">
-          <p class="lead text-center">Ask</p>
-          <p>Ask questions about Russian culture and find out where you can connect with the community in NZ.</p>
-        </div>
-      </div>
+        <div class="container">
+          <div class="col-sm-4">
+            <p class="lead text-center">Blog</p>
+            <p>Publish your own blog posts about your experiences in New Zealand and connect with other Russians.</p>
+          </div>
+
+          <div class="col-sm-4">
+            <p class="lead text-center">Comment</p>
+            <p>Share your thoughts and discuss other people’s posts.</p>
+          </div>
+
+          <div class="col-sm-4">
+            <p class="lead text-center">Ask</p>
+            <p>Ask questions about Russian culture and find out where you can connect with the community in NZ.</p>
+          </div>
+        </div> <!-- /Container -->
+      </div> <!-- /Row -->
