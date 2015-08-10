9cd4f410949e3da7ecdf998c99fe41c6d127d75b
Lena Plaksina <lena.plaksina@gmail.com>
2015-08-10 23:23:39 +1200

    Style search results page similarly to dash view, display search query at top of page

--- public_html/templates/searchresults.inc.php
+++ public_html/templates/searchresults.inc.php
@@ -1,31 +1,51 @@
-<div class="container">
-  <div class="post">
+<div class="user-info text-center">
+  <div class="container">
 
-    <h1>Search Results:</h1>
+    <div>
+      <h1 class="text-center">Search results for: "<?= $q ?>"</h1>
+    </div>
+
+  </div><!-- /.container -->
+</div><!-- /.user-info -->
+
+  <div class="container search-results">
 
     <?php if (count($posts) > 0): ?>
+      <?php foreach ($posts as $post ): ?>
+        <?= $post->loadTags() ?>
+        <div class="col-md-6 col-lg-4">
 
-      <ol>
-        <?php foreach($posts as $post): ?>
-          <li>
-            <h3><a href="./?page=post&amp;id=<?= $post->id ?>">
-              <?= $post->title; ?>
-            </a></h3>
-            <p><?= $post->content; ?></p>
-            <ul class="list-inline">
-            <?php foreach($post->getTags() as $tag): ?><!--
-              --><li><span class="label label-default"><?= $tag->tag ?></span></li><!--
-            --><?php endforeach; ?>
-            </ul>
-          </li>
-        <?php endforeach; ?>
-      </ol>
+          <div class="post-preview">
+            <h4 class="text-center post-title"><a href="./?page=post&amp;id=<?= $post->id ?>">
+            <?= $post->title ?></a></h4>
+            <div><?= substr("$post->content", 0, 388) ?><a href="./?page=post&amp;id=<?= $post->id ?>"><span>…</span></a></div>
 
+            <div class="extra">
+              <div class="tags">
+                <?php if ($post->tags == ""): ?> 
+                  no tags to display
+                <?php else: ?>
+                  <?= $post->tags ?>
+                <?php endif; ?>
+              </div>
+
+              <div>
+                <a href="./?page=post&amp;id=<?= $post->id ?>#comments"><?= count($post->comments()) ?> 
+                  <?php if (count($post->comments()) === 1): ?> 
+                    comment
+                  <?php else: ?>
+                    comments
+                  <?php endif; ?>
+                </a>
+              </div>
+            </div> <!-- /.extra -->
+           
+          </div> <!-- /.post-preview -->
+
+        </div> <!-- /.col-md-4 -->
+      <?php endforeach; ?>
     <?php else: ?>
-
       <p>That search didn't return any posts. Try again!</p>
-
     <?php endif; ?>
 
-  </div> <!-- /.post-preview -->
-</div> <!-- /.container -->
+  </div> <!-- /.container -->
